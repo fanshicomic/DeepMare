2e640ff37f75cfbb17535749725d0162f36773a4
Fanshi <fanshi.lin@go-jek.com>
2023-12-09 12:29:51 +0800

    Initial Commit

---
+++ DeepMare/DeepMareApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepMareApp.swift
+//  DeepMare
+//
+//  Created by Lin Fanshi on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DeepMareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
