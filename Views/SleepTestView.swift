18e68b57d08babd7e4f2e37f6184c21bc7e9456a
Mingyu Lei <leimy@chromium.org>
2023-12-24 01:37:10 +0900

    Add hacky speech recognition

--- Views/SleepTestView.swift
+++ Views/SleepTestView.swift
@@ -6,17 +6,29 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct SleepTestView: View {
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State var audioPlayer: AVAudioPlayer!
+
     var body: some View {
         VStack {
-            Text("睡得爽嗎")
-            Text("睡得爽嗎")
-            Text("睡得爽嗎")
-            Text("睡得爽嗎")
-            Text("睡得爽嗎")
-            Text("睡得爽嗎")
+            Text("\(speechRecognizer.result ? "💩你真棒💩" : "重复你听到的最后一个词")")
         }
         .padding()
+        .onAppear() {
+            let sound = Bundle.main.url(forResource: "盘旋", withExtension: "mp3")!
+            self.audioPlayer = try! AVAudioPlayer(contentsOf: sound)
+            self.audioPlayer.play()
+            // Hack
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                speechRecognizer.startTranscribing()
+            }
+        }
     }
+}
+
+#Preview {
+    SleepTestView()
 }
